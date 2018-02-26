62be83f6b0a060ca7904710933ca66809ccff37a
Alex Whitt <alex.joseph.whitt@gmail.com>
2018-02-26 12:10:05 -0500

    Update versions and drop deprecated packages

--- project.clj
+++ project.clj
@@ -1,12 +1,9 @@
-(defproject org.clojars.maoe/snmp4clj "0.0.1-SNAPSHOT"
+(defproject org.clojars.maoe/snmp4clj "0.0.2-SNAPSHOT"
   :description "SNMP API for Clojure"
-  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
-                 [org.snmp4j/snmp4j "1.11"]
-                 [org.clojars.maoe/funky "0.0.6"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]
-                     [lein-clojars "0.5.0-SNAPSHOT"]
-                     [autodoc "0.7.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.snmp4j/snmp4j "2.6.0"]]
+  :dev-dependencies [[autodoc "0.7.0"]]
   :namespaces [snmp4clj]
-  :repositories {"oosnmp" "https://server.oosnmp.net/dist/release"}
+  :repl-options {:init-ns user}
+  :profiles {:dev {:source-paths ["dev/src"]}}
   :main snmp4clj.examples)
