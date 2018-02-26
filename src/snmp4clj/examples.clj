4ece587cd41416a67521375f06999133393aa25c
Alex Whitt <alex.joseph.whitt@gmail.com>
2018-02-26 12:11:02 -0500

    Use `require` instead of `use` for clarity

--- src/snmp4clj/examples.clj
+++ src/snmp4clj/examples.clj
@@ -1,13 +1,14 @@
 (ns snmp4clj.examples
-  (:use [snmp4clj core session])
-  (:use [clojure.contrib core pprint])
+  (:require [snmp4clj.session :as session]
+            [snmp4clj.core :as core]
+            [clojure.pprint :as pprint])
   (:import [org.snmp4j.event ResponseListener])
   (:gen-class))
 
 (defn -main []
-  (with-snmp-session session
+  (session/with-snmp-session session
     (println "retrieve a name of the first network interface")
-    (println (snmp-get-next session "1.3.6.1.2.1.31.1.1.1.1"))
+    (println (core/snmp-get-next session "1.3.6.1.2.1.31.1.1.1.1"))
 
     ;; (println "retrieve asynchronously")
     ;; (let [listener (proxy [ResponseListener] []
@@ -15,12 +16,12 @@
     ;;                              (print (.getResponse event))
     ;;                              (println " (async)")))]
     ;;   (dotimes [_ 100]
-    ;;     (snmp-get-next session
+    ;;     (core/snmp-get-next session
     ;;                    :async listener
     ;;                    "1.3.6.1.2.1.31.1.1.1.1")))
 
     ;; (println "retrieve a status map of all network interfaces")
-    ;; (-?> (snmp-table-walk session
+    ;; (-?> (core/snmp-table-walk session
     ;;        "1.3.6.1.2.1.31.1.1.1.1"
     ;;        "1.3.6.1.2.1.31.1.1.1.2"
     ;;        "1.3.6.1.2.1.31.1.1.1.3")
@@ -28,15 +29,15 @@
 
     (println "table-walk with for-table-* utilities")
     (println
-     (pprint
-      (snmp-table-walk session
+     (pprint/pprint
+      (core/snmp-table-walk session
                        "1.3.6.1.2.1.31.1.1.1.1"
                        "1.3.6.1.2.1.31.1.1.1.2"
                        "1.3.6.1.2.1.31.1.1.1.3")))
 
     (println
-     (pprint
-      (snmp-table-walk session
+     (pprint/pprint
+      (core/snmp-table-walk session
                        :address "udp:192.168.6.57/161"
                        "1.0.8802.1.1.2.1.4.1.1.7"
                        "1.0.8802.1.1.2.1.4.1.1.9")))))
